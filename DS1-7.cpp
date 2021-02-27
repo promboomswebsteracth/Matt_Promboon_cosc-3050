098ec6a434654c2a27bbecbd50ac8d10135baf75
promboomswebsteracth <79753676+promboomswebsteracth@users.noreply.github.com>
2021-02-27 20:47:07 +0700

    Add files via upload

---
+++ DS1-7.cpp
@@ -0,0 +1,69 @@
+//COSC 3050 Spring 2021
+//Author: Matt Promboon
+//Homework () - Problem ()
+
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char** args) {
+    std::fstream myFile;
+    if (argc < 2) 
+	{
+        cout << "Required parameter filename missing.\n";
+        return 1;
+    }
+    myFile.open(args[1]);
+    if (!(myFile.is_open())) { cout << "File " << args[1] << " not found\n"; return 1; }
+    string s;
+    vector<string> strVec;
+    int count = 0;
+    while (myFile >> s) 
+	{
+        count++;
+        //cout << s << "\n";
+        strVec.push_back(s);
+    }
+    std::sort(strVec.begin(), strVec.end());
+    cout << "Num words: " << count << "\n";
+    myFile.close();
+    string current = "";
+    string previous = "";
+    string mostFr[10][2];
+	int arrRowCount;
+    int curCount = 0;
+    int maxCount = 0;
+    for (int i = 0; i < strVec.size(); i++) 
+	{
+        current = strVec[i];
+        if (current == previous) 
+		{
+            curCount++;
+        }
+        else 
+		{
+            curCount = 1;
+        }
+        if (curCount > maxCount) 
+		{
+            maxCount = curCount;
+            mostFr[arrRowCount][0] = current;
+			mostFr[arrRowCount][1] = to_string(maxCount);
+			arrRowCount++;
+			if(arrRowCount==9) arrRowCount = 0;
+        }
+        previous = current;
+    }
+    //cout << "The most freq word is " << mostfr << ". It appears " << maxcount << " times.\n";
+    return 0;
+}
